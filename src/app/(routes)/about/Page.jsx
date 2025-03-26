e2381ffd337ff31e01b70c873aced4967e7a276a
Rofson Jame Abu Siam <abu.siam610@gmail.com>
2025-03-26 12:57:22 +0600

    fix about page bug

--- src/app/(routes)/about/Page.jsx
+++ src/app/(routes)/about/Page.jsx
@@ -1,14 +1,10 @@
-import Head from 'next/head';
 import Image from 'next/image';
 import { FaGithub, FaTwitter, FaEnvelope } from 'react-icons/fa';
 
 const About = () => {
     return (
         <>
-            <Head>
-                <title>About Us - My Blog</title>
-                <meta name="description" content="Learn more about our blog and what we do." />
-            </Head>
+            
             <div className="max-w-3xl mx-auto p-6 text-center">
                 <h1 className="text-4xl font-bold mb-4">About Us</h1>
                 <div className="flex justify-center mb-4">
